6151426f1ce142a1c0b747ff6474442c73553133
Justin Campbell <justin@justincampbell.me>
2012-07-06 17:18:58 -0400

    rails new caboose-sample
    [caboose](https://github.com/JustinCampbell/caboose)

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CabooseSample::Application.config.secret_token = 'a5c3ccf057f7ba04cd04ef954fb8afbbaad9e75d84e8491b707148ed7411d4d74bffc770cbd6b49140feae1cc838836898eff213822f108d9d62ebbbfa583aec'
